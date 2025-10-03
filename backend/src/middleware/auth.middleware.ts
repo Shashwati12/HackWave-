70dbee90cb1341fc09811d8bf8a9eb6fe299addc
Sherin <sherint275@gmail.com>
2025-10-03 11:46:43 +0530

    add authentication

---
+++ backend/src/middleware/auth.middleware.ts
@@ -0,0 +1,37 @@
+import { Response, NextFunction } from 'express';
+import asyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { AuthRequest, AuthUser } from '../types/auth.types';
+
+export const protect = asyncHandler(
+  async (req: AuthRequest, res: Response, next: NextFunction) => {
+    const authHeader = req.get('authorization');
+    let token: string | undefined;
+
+    if (authHeader?.startsWith('Bearer')) {
+      token = authHeader.split(' ')[1];
+    }
+
+    if (!token) {
+      res.status(401);
+      throw new Error('Not authorized, no token');
+    }
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+
+
+      if (typeof decoded === 'object' && decoded !== null && 'id' in decoded) {
+        req.user = decoded as AuthUser;
+        next();
+      } else {
+        res.status(401);
+        throw new Error('Not authorized, token invalid');
+      }
+    } catch (error) {
+      res.status(401);
+      throw new Error('Not authorized, token failed');
+    }
+  }
+);
