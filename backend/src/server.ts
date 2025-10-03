f911a46508be4ea3fcc76acbe5de79e074af4021
Harish-Naruto <harishnarote7@gmail.com>
2025-10-03 16:21:12 +0530

    update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,5 +5,5 @@ import config from "./config";
 const port = config.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`🚀 API server listening on http://localhost:${port}/api/v1`);
+  console.log(`API server listening on http://localhost:${port}/api/v1`);
 });
