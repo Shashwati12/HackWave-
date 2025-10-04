63072df501fc3447a10a559011e2646af1a5ad13
Sherin <sherint275@gmail.com>
2025-10-04 17:10:02 +0530

    chat routes cpmpleted

---
+++ backend/src/controllers/chat.controllers.ts
@@ -0,0 +1,20 @@
+import * as chatServices from "../services/chat.service"
+import asyncHandler from 'express-async-handler';
+import type { Request, Response } from 'express';
+import { ApiError } from "../utils/appError";
+
+export const getChatRoom = asyncHandler(async(req:Request,res:Response)=>{
+    const userId = req.userId;
+    const ChatRooms = await chatServices.getChatRoom(userId);
+    res.status(200).json(ChatRooms);
+})
+
+export const getMessageByChatRoom = asyncHandler(async(req:Request,res:Response)=>{
+    const userId = req.userId
+    const roomId = req.body.roomId;
+    if(!roomId || !userId){
+        throw new ApiError("Room Id missing",401);
+    }
+    const messages = await chatServices.getChatByRoomId(userId,roomId);
+    res.status(200).json(messages);
+})
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -7,6 +7,7 @@ import { auth } from '../middleware/auth.middleware';
 import authRouter from './auth.routes';
 import registrationRouter from './registration.routes';
 import eventRouter from './event.routes';
+import chatRoute from './chat.routes';
 
 export default function routes(upload: Multer, supabase : SupabaseClient) {
   const router = Router();
@@ -17,6 +18,7 @@ export default function routes(upload: Multer, supabase : SupabaseClient) {
   router.use(auth)
   router.use('/registration',registrationRouter());
   router.use('/event', eventRouter(upload, supabase));
+  router.use('/chat',chatRoute());
   
   return router;
 }
