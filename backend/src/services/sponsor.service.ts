1abf3be94e5529cb34dc5bdb0281b9cb21662381
shrutiii <shrutijadhav4102@gmail.com>
2025-10-04 07:24:22 +0530

    refactored complete vector and sponsor routes

---
+++ backend/src/services/sponsor.service.ts
@@ -0,0 +1,70 @@
+import {prisma} from '../config/prisma.config';
+import type { sponsorEventCollab, sponsorUpdation } from '../types/sponsor.types';
+import { ApiError } from '../utils/appError';
+
+
+export const getSponsors = async() => {
+    return await prisma.sponsor.findMany();
+}
+
+export const getSponsorDescription = async(id: number) => {
+    return await prisma.sponsor.findUnique({
+        where: {id: id},
+        include: {user: true}
+    })
+}
+
+export const initiateRequest = async(senderId: number, receiverId: number) => {
+    return await prisma.request.create({
+        data: {
+            senderId,
+            receiverId
+        }
+    })
+}
+
+export const acceptSponsorship = async(payload: sponsorEventCollab) => {
+    return await prisma.eventSponsor.create({
+        data: {
+            contribution_amount: payload.contribution_amount,
+            layer: payload.layer,
+            event: {
+                connect: { id: payload.event_id }, 
+            },
+            sponsor: {
+                connect: {userId: payload.sponsor_id }, 
+            }
+        }
+    })
+}
+
+export const SponsoredEvents = async(sponsorId: number) => {
+
+    const sponsor = await prisma.sponsor.findFirst({
+        where: {userId: sponsorId}
+    })
+
+    return await prisma.eventSponsor.findMany({
+        where: {
+            sponsor_id: sponsor?.id
+        }
+    })
+}
+
+export const profileUpdate = async(sponsorId: number, pledged_amount: number) => {
+
+    const sponsor = await prisma.sponsor.findFirst({
+        where: {
+            userId: sponsorId
+        }
+    })
+
+  return await prisma.sponsor.update({
+    where: {
+        id: sponsor?.id
+    },
+    data: {
+        pledged_amount
+    }
+  })
+}
